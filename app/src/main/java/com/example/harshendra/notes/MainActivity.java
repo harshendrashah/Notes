1ea7d722a8032f543af642ca1136de78b31709ef
harshendrashah <harshendrashah812@gmail.com>
2018-06-03 14:12:23 +0530

    Added NoteEditorActivity

--- app/src/main/java/com/example/harshendra/notes/MainActivity.java
+++ app/src/main/java/com/example/harshendra/notes/MainActivity.java
@@ -1,9 +1,46 @@
 package com.example.harshendra.notes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    static ArrayList<String> notes = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.functions_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.add_note) {
+
+            Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+
+            startActivity(intent);
+
+            return true;
+
+        }
+
+        return false;
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
